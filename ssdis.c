9d733af8e4937fe5de5f810a8fac6a2c87e610de
Erick Bauman <erick.bauman@utdallas.edu>
2018-09-17 20:16:44 -0500

    Added initial memory management for required metadata
    Now the handle used by the superset disassembler wraps Capstone's
    handle, but also stores some additional data of its own.
    
    Unfortunately it can't use the same memory allocation functions
    as Capstone, perhaps because they aren't exported.  Strangely, the
    symbols are present, but when trying to call the functions
    directly, as they are called in Capstone, it crashes because the
    actual functions are function pointers and are not called directly
    in Capstone.  I'm not sure how important unifying the memory
    management is, it's just that we may need to replace the standard
    allocator since we want the superset disassembler to operate
    outside the standard operations of a binary.

--- ssdis.c
+++ ssdis.c
@@ -1,16 +1,38 @@
 #include "ssdis.h"
 
+typedef struct ss_handle{
+	csh cs_handle; // Opaque handle for Capstone
+	uint8_t* disasm_map; // Pointer to array tracking offsets disassembled
+	uint8_t* curr_offset; // Starting offset for current linear disassembly
+	uint8_t curr_size; // Current size of bytes based on starting offset
+} ss_handle;
+
 void ss_open(cs_arch arch, cs_mode mode, csh* handle){
-	cs_open(arch, mode, handle);	
+	csh cs_handle;
+	cs_open(arch, mode, &cs_handle);
+	ss_handle* h = malloc(sizeof(ss_handle));
+	*handle = (uintptr_t)h;
+	h->cs_handle = cs_handle;
+	/* Start data uninitialized until we get the code */
+	h->disasm_map = 0;
+	h->curr_offset = 0;
+	h->curr_size = 0;
 }
 
-bool ss_disasm_iter(csh handle, const uint8_t **code, size_t* code_size, uint64_t* address, cs_insn* insn){
-	if( cs_disasm_iter(handle, code, code_size, address, insn) ){
+bool ss_disasm_iter(csh handle, const uint8_t **code, size_t* code_size,
+		uint64_t* address, cs_insn* insn){
+	ss_handle* h = (ss_handle*)handle;
+	if( cs_disasm_iter(h->cs_handle, code, code_size, address, insn) ){
 		return true;
 	}
 	return false;
 }  
 
 void ss_close(csh* handle){
-	cs_close(handle);
+	ss_handle* h = (ss_handle*)(*handle);
+	cs_close(&(h->cs_handle));
+	if( h->disasm_map ){
+		free(h->disasm_map);
+	}
+	free(h);
 }
