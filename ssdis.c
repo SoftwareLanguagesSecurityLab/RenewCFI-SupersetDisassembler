e5a5da4153bab671a25e27b16e78c3eaf2b49f30
Erick Bauman <erick.bauman@utdallas.edu>
2018-09-19 17:38:33 -0500

    Implemented returning jmp instruction for encountered offsets
    Now the superset disassembly algorithm should be working correctly,
    although it behaves slightly differently from the previous
    implementation.  In this case, instead of returning a sort of NULL
    value when we try to disassemble an already encountered offset, I
    instead decided to return a jmp instruction that jumps to itself.
    This allows for a rewriter using this to rewrite that jmp to point
    to the actual address (the first location we encountered that
    offset) without using a special case.  The previous implementation
    would just return None (in python), which required the code calling
    the disassembler to distinguish these cases.
    
    The algorithm also only inserts a jmp instruction when at least one
    valid instruction was encountered starting from each offset.  This
    prevents junk jmp instructions from being inserted, and simply uses
    the jmp instructions to tie the different disassembled "layers"
    (from different offsets) together.
    
    The starting offsets are chosen by simply incrementing the starting
    offset by one until an unencountered offset is found.  While this
    is a natural way to approach this, it does have an impact on what
    contiguous chunks of code are found.  Depending on the way
    instructions overlap, this may result in many tiny sequences of
    instructions at the end of the disassembly that all jump to longer
    sequences near the start of the disassembly.  If one of these tiny
    sequences is a valid target, this would add an extra unconditional
    jump of overhead.  Whether this is a big deal, and what approaches
    could be taken to try to find the "intended" sequences and merge
    them, is a separate issue, but I do think it's important to
    consider the implications of each implementation decision on
    overhead.

--- ssdis.c
+++ ssdis.c
@@ -4,10 +4,16 @@ typedef struct ss_handle{
 	csh cs_handle; // Opaque handle for Capstone
 	uint8_t* disasm_map; // Pointer to array tracking visited offsets
 	size_t orig_size; // Size of the initial code
+	uint64_t orig_addr; // Original base address
 	const uint8_t* curr_offset; // Starting offset for linear disassembly
 	size_t curr_size; // Current size of bytes based on starting offset
 	uint64_t curr_addr; // Current address based on starting offset
 } ss_handle;
+
+/* Template bytes for direct unconditional jump instruction.
+   The instruction jumps to itself. */
+const uint8_t* jmp_template = "\xe9\xfb\xff\xff\xff";
+size_t jmp_template_size = 5;
 
 void ss_open(cs_arch arch, cs_mode mode, csh* handle){
 	csh cs_handle;
@@ -26,20 +32,52 @@ void ss_open(cs_arch arch, cs_mode mode, csh* handle){
 bool ss_disasm_iter(csh handle, const uint8_t **code, size_t* code_size,
 		uint64_t* address, cs_insn* insn){
 	ss_handle* h = (ss_handle*)handle;
+	uint64_t map_offset;
 	/* Check whether we have set cur_offset.  If not, we are starting
 		a new disassembly, and need to set the start offset and size */
 	if( !(h->curr_offset) ){
 		h->orig_size = *code_size;
+		h->orig_addr = *address;
 		h->curr_offset = *code;
 		h->curr_size = *code_size;
 		h->curr_addr = *address;
 		//Allocate as many bytes as the size of the original code
-		h->disasm_map = malloc(h->orig_size);
+		h->disasm_map = calloc(h->orig_size,1);
 	}
-	if( cs_disasm_iter(h->cs_handle, code, code_size, address, insn) ){
+	map_offset = *address - h->orig_addr;
+	// Check that the instruction has not been visited and is valid
+	if( !h->disasm_map[map_offset] &&
+	  cs_disasm_iter(h->cs_handle, code, code_size, address, insn) ){
+		h->disasm_map[map_offset] = 1;
 		return true;
 	}else if(h->curr_size > 1){
-		// Loop until a valid instruction is reached or we hit the end
+		// If we have already encountered this offset,
+		// return a jump instruction to the offset and try next offset
+		if( h->disasm_map[map_offset] ){
+			// Return the jump template jumping to itself, since
+			// the target address is at its own address.
+			// We must copy the template and its size since
+			// cs_disasm_iter has the side-effect of changing
+			// the input pointers.
+			const uint8_t* jmp_ptr = jmp_template;
+			size_t jmp_sze = jmp_template_size; 
+			cs_disasm_iter(h->cs_handle,
+				&jmp_ptr, &jmp_sze, address, insn);	
+			// Loop until we encounter a new offset
+			// so that we don't insert useless jmps
+			do{
+				h->curr_offset++;
+				h->curr_size--;
+				h->curr_addr++;
+				map_offset = h->curr_addr - h->orig_addr;
+			}while( h->disasm_map[map_offset] );
+			*code = h->curr_offset;
+			*code_size = h->curr_size;
+			*address = h->curr_addr;
+			return true;
+		}
+		// Loop until a valid instruction at an address we have not
+		// encountered yet is reached or we hit the end
 		do{
 			if( h->curr_size <= 1 ) return false;
 			h->curr_offset++;
@@ -48,7 +86,10 @@ bool ss_disasm_iter(csh handle, const uint8_t **code, size_t* code_size,
 			*code = h->curr_offset;
 			*code_size = h->curr_size;
 			*address = h->curr_addr;
-		}while(!cs_disasm_iter(h->cs_handle, code, code_size, address, insn));
+			map_offset = h->curr_addr - h->orig_addr;
+		}while( h->disasm_map[map_offset] || !cs_disasm_iter(h->cs_handle,
+					code, code_size, address, insn));
+		h->disasm_map[map_offset] = 1;
 		return true;
 	}else{
 		return false;
