597b272b16b2533bbafdb891ca776fd8a67ac679
Erick Bauman <erick.bauman@utdallas.edu>
2018-09-18 20:44:20 -0500

    Added disassembly from successive offsets, but with duplication
    Now it disassembles up until it hits a bad instruction or the end
    of the byte sequence, and then proceeds to disassemble from the
    next starting offset.  I still need to remove duplicates of already
    disassembled offsets, and then stitch the different offsets
    together with jump instructions.

--- driver.c
+++ driver.c
@@ -6,16 +6,16 @@ int main(int argc, char** argv){
 
 	cs_insn *insn = cs_malloc(handle);
 	
-	uint8_t *code = "\x90\x91\x92";
-	size_t code_size = 3;	// size of @code buffer above
-	uint64_t address = 0x1000;	// address of first instruction to be disassembled
+	uint8_t *code = "\x48\x89\xf8\x48\x83\xf8\x00\x74\x19\x48\xb8\x4e\x07\x40\x00\x00\x00\x00\x00\xc3\x6d\x6f\x64\x65\x3a\x20\x25\x64\x0a\x00\x25\x73\x0a\x00\x48\xb8\x44\x07\x40\x00\x00\x00\x00\x00\xc3";
+	size_t code_size = 45;	// size of @code buffer above
+	uint64_t address = 0x400730;	// address of first instruction to be disassembled
 
 	// disassemble one instruction a time & store the result into @insn variable above
     	while(ss_disasm_iter(handle, (const uint8_t**)&code, &code_size, &address, insn)) {
         	// analyze disassembled instruction in @insn variable ...
         	// NOTE: @code, @code_size & @address variables are all updated
         	// to point to the next instruction after each iteration.
-		printf("%s\t%s\n", insn->mnemonic, insn->op_str);
+		printf("0x%lx: %s\t%s\n", insn->address, insn->mnemonic, insn->op_str);
     	}
 	
 	cs_free(insn, 1);
--- ssdis.c
+++ ssdis.c
@@ -2,9 +2,11 @@
 
 typedef struct ss_handle{
 	csh cs_handle; // Opaque handle for Capstone
-	uint8_t* disasm_map; // Pointer to array tracking offsets disassembled
-	uint8_t* curr_offset; // Starting offset for current linear disassembly
-	uint8_t curr_size; // Current size of bytes based on starting offset
+	uint8_t* disasm_map; // Pointer to array tracking visited offsets
+	size_t orig_size; // Size of the initial code
+	const uint8_t* curr_offset; // Starting offset for linear disassembly
+	size_t curr_size; // Current size of bytes based on starting offset
+	uint64_t curr_addr; // Current address based on starting offset
 } ss_handle;
 
 void ss_open(cs_arch arch, cs_mode mode, csh* handle){
@@ -17,15 +19,40 @@ void ss_open(cs_arch arch, cs_mode mode, csh* handle){
 	h->disasm_map = 0;
 	h->curr_offset = 0;
 	h->curr_size = 0;
+	h->orig_size = 0;
+	h->curr_addr = 0;
 }
 
 bool ss_disasm_iter(csh handle, const uint8_t **code, size_t* code_size,
 		uint64_t* address, cs_insn* insn){
 	ss_handle* h = (ss_handle*)handle;
+	/* Check whether we have set cur_offset.  If not, we are starting
+		a new disassembly, and need to set the start offset and size */
+	if( !(h->curr_offset) ){
+		h->orig_size = *code_size;
+		h->curr_offset = *code;
+		h->curr_size = *code_size;
+		h->curr_addr = *address;
+		//Allocate as many bytes as the size of the original code
+		h->disasm_map = malloc(h->orig_size);
+	}
 	if( cs_disasm_iter(h->cs_handle, code, code_size, address, insn) ){
 		return true;
+	}else if(h->curr_size > 1){
+		// Loop until a valid instruction is reached or we hit the end
+		do{
+			if( h->curr_size <= 1 ) return false;
+			h->curr_offset++;
+			h->curr_size--;
+			h->curr_addr++;
+			*code = h->curr_offset;
+			*code_size = h->curr_size;
+			*address = h->curr_addr;
+		}while(!cs_disasm_iter(h->cs_handle, code, code_size, address, insn));
+		return true;
+	}else{
+		return false;
 	}
-	return false;
 }  
 
 void ss_close(csh* handle){
