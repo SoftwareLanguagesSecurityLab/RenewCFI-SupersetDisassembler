5eefc3fe3694d02297412a5a98972afb4523832c
Erick Bauman <erick.bauman@utdallas.edu>
2018-09-20 21:09:18 -0500

    Restructured disassembly code, added returning hlt on invalid inst
    Now the returned disassembly terminates sequences that end in an
    invalid instruction with hlt, and sequences that end in a
    previously visited address with a jmp to itself.  I think I still
    will need to differentiate these "special" instructions from real
    instructions so they can be cleanly distinguished by anything
    using this.
    
    Since we likely don't need sequences that terminate in an invalid
    instruction (past the last unconditional control flow instruction),
    I need to make it easy for code generation that uses this to trim
    away these bad sequences.  I still think it's fine and maybe even
    desirable to return these suspect instructions, depending on what
    kind of assumptions we are making about the binary, but under
    normal conditions we will want to eliminate these when doing
    rewriting for security reasons.  However, I think that step can be
    done in the code generation phase, but I do need to provide a way
    to identify the "special" instructions so that it's very clear what
    instructions were in the original and which were added to mark
    the ends of sequences.

--- ssdis.c
+++ ssdis.c
@@ -8,12 +8,17 @@ typedef struct ss_handle{
 	const uint8_t* curr_offset; // Starting offset for linear disassembly
 	size_t curr_size; // Current size of bytes based on starting offset
 	uint64_t curr_addr; // Current address based on starting offset
+	bool valid_seq; // Whether we found at least 1 new valid instruction
 } ss_handle;
 
 /* Template bytes for direct unconditional jump instruction.
    The instruction jumps to itself. */
 const uint8_t* jmp_template = "\xe9\xfb\xff\xff\xff";
 size_t jmp_template_size = 5;
+/* Template bytes for halt instruction.  Used after the last instruction, or
+   to terminate sequences that end in invalid instructions. */
+const uint8_t* hlt_template = "\xf4";
+size_t hlt_template_size = 1;
 
 void ss_open(cs_arch arch, cs_mode mode, csh* handle){
 	csh cs_handle;
@@ -27,6 +32,7 @@ void ss_open(cs_arch arch, cs_mode mode, csh* handle){
 	h->curr_size = 0;
 	h->orig_size = 0;
 	h->curr_addr = 0;
+	h->valid_seq = false;
 }
 
 bool ss_disasm_iter(csh handle, const uint8_t **code, size_t* code_size,
@@ -49,48 +55,71 @@ bool ss_disasm_iter(csh handle, const uint8_t **code, size_t* code_size,
 	if( !h->disasm_map[map_offset] &&
 	  cs_disasm_iter(h->cs_handle, code, code_size, address, insn) ){
 		h->disasm_map[map_offset] = 1;
+		h->valid_seq = true;
 		return true;
-	}else if(h->curr_size > 1){
-		// If we have already encountered this offset,
-		// return a jump instruction to the offset and try next offset
-		if( h->disasm_map[map_offset] ){
-			// Return the jump template jumping to itself, since
-			// the target address is at its own address.
-			// We must copy the template and its size since
-			// cs_disasm_iter has the side-effect of changing
-			// the input pointers.
-			const uint8_t* jmp_ptr = jmp_template;
-			size_t jmp_sze = jmp_template_size; 
-			cs_disasm_iter(h->cs_handle,
-				&jmp_ptr, &jmp_sze, address, insn);	
+	}else if(h->curr_size > 0){
+		if( h->valid_seq ){
+			// If this instruction was preceded by a valid sequence,
+			// then we should return a special instruction, but
+			// after returning a special instruction, the valid
+			// sequence has ended.
+			h->valid_seq = false;
+			// If we have already encountered this offset,
+			// return a jump instruction to the offset
+			// and try next offset
+			if( h->disasm_map[map_offset] ){
+				// Return a jmp jumping to itself, since
+				// the target address is at its own address.
+				// We must copy the template and its size since
+				// cs_disasm_iter has the side-effect of
+				// changing the input pointers.
+				const uint8_t* jmp_ptr = jmp_template;
+				size_t jmp_sze = jmp_template_size; 
+				cs_disasm_iter(h->cs_handle,
+					&jmp_ptr, &jmp_sze, address, insn);	
+			}else{
+				// If we get here, we encountered an invalid
+				// instruction.  Therefore, insert a hlt to
+				// ensure safe execution.
+				const uint8_t* hlt_ptr = hlt_template;
+				size_t hlt_sze = hlt_template_size;
+				cs_disasm_iter(h->cs_handle,
+					&hlt_ptr, &hlt_sze, address, insn);	
+			}
 			// Loop until we encounter a new offset
 			// so that we don't insert useless jmps
-			do{
+			//do{
 				h->curr_offset++;
 				h->curr_size--;
 				h->curr_addr++;
-				map_offset = h->curr_addr - h->orig_addr;
-			}while( h->disasm_map[map_offset] );
+			//	map_offset = h->curr_addr - h->orig_addr;
+			//}while( h->disasm_map[map_offset] && h->curr_size > 1 );
 			*code = h->curr_offset;
 			*code_size = h->curr_size;
 			*address = h->curr_addr;
 			return true;
-		}
-		// Loop until a valid instruction at an address we have not
-		// encountered yet is reached or we hit the end
-		do{
-			if( h->curr_size <= 1 ) return false;
-			h->curr_offset++;
-			h->curr_size--;
-			h->curr_addr++;
-			*code = h->curr_offset;
-			*code_size = h->curr_size;
-			*address = h->curr_addr;
-			map_offset = h->curr_addr - h->orig_addr;
-		}while( h->disasm_map[map_offset] || !cs_disasm_iter(h->cs_handle,
+		}else{
+			// If this instruction was not preceded by a valid
+			// sequence, then we should skip forward, because we
+			// don't want duplicate hlt or jmp instructions.
+			// Loop until a valid instruction at an address we have
+			// not encountered yet is reached or we hit the end
+			do{
+				if( h->curr_size <= 1 ) return false;
+				h->curr_offset++;
+				h->curr_size--;
+				h->curr_addr++;
+				*code = h->curr_offset;
+				*code_size = h->curr_size;
+				*address = h->curr_addr;
+				map_offset = h->curr_addr - h->orig_addr;
+			}while( h->disasm_map[map_offset] ||
+				!cs_disasm_iter(h->cs_handle,
 					code, code_size, address, insn));
-		h->disasm_map[map_offset] = 1;
-		return true;
+			h->disasm_map[map_offset] = 1;
+			h->valid_seq = true;
+			return true;
+		}
 	}else{
 		return false;
 	}
